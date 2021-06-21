c51c07217ce755f86d3740b2a658c72be8ac3e11
Diogo Sales <diogosalesdev@gmail.com>
2021-06-21 10:06:03 -0300

    First commit

---
+++ src/CreateCourseService.ts
@@ -0,0 +1,15 @@
+
+interface CourseProps {
+  name: string;
+  duration: number;
+  educator: string;
+}
+
+class CreateCourseService {
+
+  execute({ name, duration, educator}: CourseProps) {
+    console.log(name, duration, educator)
+  }
+}
+
+export default new CreateCourseService()
