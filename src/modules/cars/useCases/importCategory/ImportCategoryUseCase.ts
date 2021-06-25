e76dd5e424d88165ace67accd5df4a64677b7778
Diogo Sales <diogosalesdev@gmail.com>
2021-06-25 13:46:17 -0300

    File uploaded removed

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -29,6 +29,7 @@ class ImportCategoryUseCase {
           });
         })
         .on("end", () => {
+          fs.promises.unlink(file.path); // removendo o arquivo importado
           resolve(categories);
         })
         .on("error", err => {
