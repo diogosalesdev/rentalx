c53c65fc63063c26e1394268d5b781242e166458
Diogo Sales <diogosalesdev@gmail.com>
2021-07-13 16:07:49 -0300

    Password encrypted

--- src/modules/accounts/dtos/IUsersRepositoryDTO.ts
+++ src/modules/accounts/dtos/IUsersRepositoryDTO.ts
@@ -1,7 +1,9 @@
+import { User } from "../entities/User";
 import { ICreateUserDTO } from "./ICreateUserDTO";
 
 interface IUsersRepositoryDTO {
   create(data: ICreateUserDTO): Promise<void>;
+  findByEmail(email: string): Promise<User>;
 }
 
 export { IUsersRepositoryDTO };
